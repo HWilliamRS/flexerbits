2799ea0d80f0ed04362392157abba9b719bbf144
Héctor William Rodríguez Suárez <hwilliamrs@yahoo.com>
2024-10-27 10:57:43 -0400

    - Tailwind initial configuration     - Layout initial configuration     - Index page initial configuration

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-gray-900 text-white p-4 text-center">
+    <p>© 2024 FlexerBits. All rights reserved.</p>
+  </footer>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-purple-800 text-white p-4">
+    <h1 class="text-2xl font-bold">FlexerBits header</h1>
+  </header>
+</template>
