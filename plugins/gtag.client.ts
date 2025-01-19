e735ff76ea91d10c8d70dacf6a5df52f744af470
Héctor William Rodríguez Suárez <hwilliamrs@yahoo.com>
2025-01-19 17:56:32 -0400

    Updating gtat plugin configuration

--- plugins/gtag.client.ts
+++ plugins/gtag.client.ts
@@ -1,15 +1,17 @@
 // plugins/gtag.client.ts
 import { defineNuxtPlugin } from '#app'
+import { useRouter } from 'vue-router'
 import VueGtag from 'vue-gtag'
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Only enable on production builds (optional)
   const isProduction = process.env.NODE_ENV === 'production'
+  const router = useRouter()
 
   nuxtApp.vueApp.use(VueGtag, {
     config: { id: 'G-R3EQ9GXZYY', params: { send_page_view: true } },
     pageTrackerScreenviewEnabled: true, 
     enabled: true,
     appName: 'FlexerBits'
-  })
+  }, router)
 })
